6e1ae9da80b9139beed93c7dcc37bc5f3eeea756
HCYS01 <cgs.info01@gmail.com>
2021-05-21 16:09:41 +0700

    Create form_data.php

---
+++ form_data.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<title>2nd Page</title>
+	</head>
+	<body>
+		<H1>WELCOME</H1>
+
+		<?php 
+		echo "Area " . $_GET['kodearea'] . " dan Nomor " . $_GET['nomor'];
+		?>
+	</body>
+</html>
